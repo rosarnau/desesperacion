0ab821ee7f22c157ee5ec2118099fc915485c6ae
[Rosa][Arnau] <[rarnau@uoc.edu]>
2023-12-25 23:50:10 +0100

    new files reactive article

--- src/app/article-new-reactive/article-new-reactive.component.ts
+++ src/app/article-new-reactive/article-new-reactive.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
+
+// Definir la función del validador personalizado aquí
+function NameArticleValidator(control: AbstractControl): ValidationErrors | null {
+  const forbiddenNames = ['Prueba', 'test', 'mock', 'fake']; // Agrega los nombres prohibidos según tus necesidades
+
+  // Verificar si el valor del control está en la lista de nombres prohibidos
+  if (control.value && forbiddenNames.includes(control.value.toLowerCase())) {
+    return { forbiddenName: true }; // Devuelve un objeto con la propiedad forbiddenName
+  }
+
+  return null; // La validación es exitosa
+}
 
 @Component({
   selector: 'app-article-new-reactive',
   templateUrl: './article-new-reactive.component.html',
-  styleUrl: './article-new-reactive.component.css'
+  styleUrls: ['./article-new-reactive.component.css']
 })
-export class ArticleNewReactiveComponent {
+export class ArticleNewReactiveComponent implements OnInit {
+  articleForm: FormGroup;
+  submitted = false; // Rastrear si se ha enviado el formulario
 
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.articleForm = this.fb.group({
+      nombre: ['', Validators.required],
+      precio: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/), Validators.min(0.1)]],
+      imagenUrl: ['', [Validators.required, Validators.pattern(/^(https?:\/\/)?\S+$/)]],
+      enVenta: [false]
+    });
+  }
+
+  // Obtener controles del formulario para mostrar mensajes de error
+  get formControls() {
+    return this.articleForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true; // Marca que se ha enviado el formulario
+
+    // Valida si el formulario es válido antes de realizar acciones
+    if (this.articleForm.valid) {
+      
+      console.log(this.articleForm.value);
+    } else {
+      // Puedes mostrar un mensaje de error
+      console.log("Formulario no válido. Por favor, complete todos los campos obligatorios y asegúrese de que el precio sea mayor o igual a 0,1€.");
+    }
+  }
 }
+
